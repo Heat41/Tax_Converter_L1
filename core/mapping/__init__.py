9f224f945f08ec93b16cae2b83df3642b30fca65
Muhammad Ihsan Kamil <97449763+Heat41@users.noreply.github.com>
2026-09-06 18:34:51 +0700

    feat: expose legacy 1770-IV mapping API

--- core/mapping/__init__.py
+++ core/mapping/__init__.py
@@ -1,3 +1,9 @@
 from .harta_mapper import HartaMappingResult, CoretaxHartaMapper
+from .legacy_1770iv import Legacy1770IVMapper, Legacy1770IVRow
 
-__all__ = ["CoretaxHartaMapper", "HartaMappingResult"]
+__all__ = [
+    "CoretaxHartaMapper",
+    "HartaMappingResult",
+    "Legacy1770IVMapper",
+    "Legacy1770IVRow",
+]
