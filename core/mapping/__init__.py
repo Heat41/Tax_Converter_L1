47c43228fd56f4c4f07bbb3d7777c8097568063c
Muhammad Ihsan Kamil <97449763+Heat41@users.noreply.github.com>
2026-09-06 18:48:54 +0700

    feat: expose real worksheet mapping API

--- core/mapping/__init__.py
+++ core/mapping/__init__.py
@@ -1,9 +1,16 @@
+from .eform_reference import EFORM_ASSET_REFERENCES, EFormAssetReference, find_eform_reference
 from .harta_mapper import HartaMappingResult, CoretaxHartaMapper
 from .legacy_1770iv import Legacy1770IVMapper, Legacy1770IVRow
+from .worksheet_harta_mapper import WorksheetHartaMapper, WorksheetHartaRow
 
 __all__ = [
     "CoretaxHartaMapper",
     "HartaMappingResult",
     "Legacy1770IVMapper",
     "Legacy1770IVRow",
+    "EFormAssetReference",
+    "EFORM_ASSET_REFERENCES",
+    "find_eform_reference",
+    "WorksheetHartaMapper",
+    "WorksheetHartaRow",
 ]
