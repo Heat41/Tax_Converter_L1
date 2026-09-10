fe99e229fdf2868005d63db83443f8aeb94ee3bb
Muhammad Ihsan Kamil <97449763+Heat41@users.noreply.github.com>
2026-09-10 14:55:23 +0700

    chore: use fine tuned Lampiran II renderer in visual test

--- scripts/export_1770_lampiran_ii_test.py
+++ scripts/export_1770_lampiran_ii_test.py
@@ -9,7 +9,7 @@ if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
 from core.legacy_1770 import Legacy1770DocumentService
-from core.legacy_1770_lampiran_ii import Legacy1770LampiranIIService
+from core.legacy_1770_lampiran_ii_finetuned import Legacy1770LampiranIIService
 from core.legacy_pdf_template import DEFAULT_TEMPLATE_PATH
 
 
