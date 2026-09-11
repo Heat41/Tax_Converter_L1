7f5b656ce930e4d985be68672aa0de6136a667b8
Muhammad Ihsan Kamil <97449763+Heat41@users.noreply.github.com>
2026-09-11 09:34:30 +0700

    style: enlarge Lampiran I employment identity font

--- core/legacy_1770_lampiran_i_finetuned.py
+++ core/legacy_1770_lampiran_i_finetuned.py
@@ -22,7 +22,7 @@ class Legacy1770LampiranIService(BaseLegacy1770LampiranIService):
     """
 
     # Perbesar identitas Nama/NPWP pemberi kerja tanpa mengubah geometri sel.
-    C_IDENTITY_FONT_SIZE = 5.9
+    C_IDENTITY_FONT_SIZE = 6.5
 
     # Koordinat master Lampiran I halaman 1, origin kiri-atas.
     PAGE1_YEAR_RECT: Rect = (457.84, 23.90, 579.60, 43.82)
