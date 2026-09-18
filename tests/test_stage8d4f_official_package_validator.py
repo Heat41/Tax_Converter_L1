7e816c1df8f2a53136aedba7c7344910c403a915
Muhammad Ihsan Kamil <97449763+Heat41@users.noreply.github.com>
2026-09-18 13:59:45 +0700

    refactor: generalize sample fixtures in tests/test_stage8d4f_official_package_validator.py

--- tests/test_stage8d4f_official_package_validator.py
+++ tests/test_stage8d4f_official_package_validator.py
@@ -41,7 +41,7 @@ def _templates(directory: Path):
 def _package():
     package = ReverseCoretaxPackage(
         npwp="6101015612710001",
-        nama_wp="EVY BACHTIAR",
+        nama_wp="WAJIB PAJAK TEST",
         tahun_pajak=2025,
         revision=3,
         snapshot_hash="abc123",
@@ -55,11 +55,11 @@ def _package():
             tahun_perolehan=2025,
             nilai=391742658,
             nomor_akun_keterangan="116801005138508",
-            atas_nama="DR EVY BACHTIAR SPOG",
+            atas_nama="DR WAJIB PAJAK TEST SPOG",
             nama_bank="BRI",
             official_metadata={
                 "account_number": "116801005138508",
-                "account_on_behalf_of": "DR EVY BACHTIAR SPOG",
+                "account_on_behalf_of": "DR WAJIB PAJAK TEST SPOG",
                 "bank_name": "BRI",
                 "country": "Indonesia",
                 "year": 2025,
